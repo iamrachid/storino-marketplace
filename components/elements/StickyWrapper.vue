fbd382b62486b347fe5c40eb8a8da2335369d3a5
Imad.chaichaa@linux <imadchai2@gmail.com>
2022-03-19 15:54:03 +0100

    Scrolling fix

--- components/elements/StickyWrapper.vue
+++ components/elements/StickyWrapper.vue
@@ -1,104 +1,100 @@
 <template>
-	<div class="sticky-wrapper" ref="stickyRef">
-		<slot></slot>
-	</div>
+  <div
+      className="sticky-wrapper"
+      ref="stickyRef"
+  >
+    <slot></slot>
+  </div>
 </template>
 
 <script>
 export default {
-	props: {
-		direction: {
-			type: String,
-			default: 'normal'
-		},
-		type: {
-			type: String,
-			default: 'desktop'
-		},
-		breakpoint: {
-			type: Number,
-			default: 992
-		},
-		stickyClass: {
-			type: String,
-			default: 'sticky-header'
-		},
-		offsetTop: {
-			type: Number,
-			default: 300
-		},
-		top: {
-			type: Number,
-		}
-	},
-	data: function () {
-		return {
-			isSticky: false,
-			stickyContent: null,
-			stickyHeight: 0,
-			prevPos: 0,
-			scrollDir: 'down',
-			terminal: this.offsetTop
-		};
-	},
-	mounted: function () {
-		this.stickyWrapperHandler();
-
-		if ( this.stickyClass === 'sticky-header' && this.terminal === 300 && document.querySelector( 'main' ) ) {
-			this.terminal = document.querySelector( 'main' ).offsetTop;
-		}
-		window.addEventListener( 'scroll', this.stickyWrapperHandler, true );
-		window.addEventListener( 'resize', this.stickyWrapperHandler, true );
-	},
-	destroyed: function () {
-		window.removeEventListener( 'scroll', this.stickyWrapperHandler, true );
-		window.removeEventListener( 'resize', this.stickyWrapperHandler, true );
-	},
-	methods: {
-		toggleStickyContent: function ( state = true ) {
-			if ( state ) {
-				this.isSticky = true;
-				this.stickyContent.classList.add( 'fixed' );
-				this.$refs.stickyRef.setAttribute( 'style', 'height: ' + this.stickyHeight + 'px' );
-				this.$refs.stickyRef.querySelector( '.' + this.stickyClass ).style.top = this.top + 'px';
-			} else {
-				this.isSticky = false;
-				this.stickyContent.classList.remove( 'fixed' );
-				this.$refs.stickyRef.removeAttribute( 'style' );
-			}
-		},
-		stickyWrapperHandler: function () {
-			this.stickyContent = this.$refs.stickyRef.querySelector( '.' + this.stickyClass );
-			this.stickyHeight = this.stickyContent && this.$refs.stickyRef.querySelector( '.' + this.stickyClass ).offsetHeight;
-			this.scrollDir = window.scrollY - this.prevPos > 0 ? 'down' : 'up';
-
-			if ( this.type === 'desktop' ) {
-				if ( window.scrollY > this.terminal && window.innerWidth > this.breakpoint ) {
-					if ( !this.isSticky ) this.toggleStickyContent( true );
-
-					if ( this.direction === 'normal' || this.scrollDir === this.direction )
-            this.toggleStickyContent( true );
-					else
-            this.toggleStickyContent( false );
-				} else {
-					this.toggleStickyContent( false );
-				}
-			}
-
-			if ( this.type === 'mobile' ) {
-				if ( window.scrollY > this.terminal && window.innerWidth < this.breakpoint ) {
-					if ( !this.isSticky ) this.toggleStickyContent( true );
-					if ( this.direction === 'normal' || this.scrollDir === this.direction )
-						this.stickyContent.classList.add( 'fixed' );
-					else
-            this.stickyContent.classList.remove( 'fixed' );
-				} else {
-					this.toggleStickyContent( false );
-				}
-			}
-
-			this.prevPos = window.scrollY;
-		}
-	}
+  props: {
+    direction: {
+      type: String,
+      default: 'normal'
+    },
+    type: {
+      type: String,
+      default: 'desktop'
+    },
+    breakpoint: {
+      type: Number,
+      default: 992
+    },
+    stickyClass: {
+      type: String,
+      default: 'sticky-header'
+    },
+    offsetTop: {
+      type: Number,
+      default: 300
+    },
+    top: {
+      type: Number,
+    }
+  },
+  data: function () {
+    return {
+      isSticky: false,
+      stickyContent: null,
+      stickyHeight: 0,
+      prevPos: 0,
+      scrollDir: 'down',
+      terminal: this.offsetTop
+    };
+  },
+  mounted: function () {
+    this.stickyWrapperHandler();
+    if (this.stickyClass === 'sticky-header' && this.terminal === 300 && document.querySelector('main')) {
+      this.terminal = document.querySelector('main').offsetTop;
+    }
+    window.addEventListener('scroll', this.stickyWrapperHandler, true);
+    window.addEventListener('resize', this.stickyWrapperHandler, true);
+  },
+  destroyed: function () {
+    window.removeEventListener('scroll', this.stickyWrapperHandler, true);
+    window.removeEventListener('resize', this.stickyWrapperHandler, true);
+  },
+  methods: {
+    toggleStickyContent: function (state = true) {
+      if (state) {
+        this.isSticky = true;
+        this.stickyContent.classList.add('fixed');
+        this.$refs.stickyRef.setAttribute('style', 'height: ' + this.stickyHeight + 'px');
+        this.$refs.stickyRef.querySelector('.' + this.stickyClass).style.top = this.top + 'px';
+      } else {
+        this.isSticky = false;
+        this.stickyContent.classList.remove('fixed');
+        this.$refs.stickyRef.removeAttribute('style');
+      }
+    },
+    stickyWrapperHandler: function () {
+      this.stickyContent = this.$refs.stickyRef.querySelector('.' + this.stickyClass);
+      this.stickyHeight = this.stickyContent && this.$refs.stickyRef.querySelector('.' + this.stickyClass).offsetHeight;
+      this.scrollDir = window.scrollY - this.prevPos > 0 ? 'down' : 'up';
+      if (this.type === 'desktop') {
+        if (window.scrollY > this.terminal && window.innerWidth > this.breakpoint) {
+          if (!this.isSticky) this.toggleStickyContent(true);
+          if (this.direction === 'normal' || this.scrollDir === this.direction)
+            this.stickyContent.classList.add('fixed');
+          else this.stickyContent.classList.remove('fixed');
+        } else {
+          this.toggleStickyContent(false);
+        }
+      }
+      if (this.type === 'mobile') {
+        if (window.scrollY > this.terminal && window.innerWidth < this.breakpoint) {
+          if (!this.isSticky) this.toggleStickyContent(true);
+          if (this.direction === 'normal' || this.scrollDir === this.direction)
+            this.stickyContent.classList.add('fixed');
+          else this.stickyContent.classList.remove('fixed');
+        } else {
+          this.toggleStickyContent(false);
+        }
+      }
+      this.prevPos = window.scrollY;
+    }
+  }
 };
 </script>
