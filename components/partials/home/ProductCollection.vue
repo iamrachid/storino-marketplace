399f4c3535c222c71351b2e0a675a470eaadb960
Imad Chaichaa@Windows <Imadchai2@gmail.com>
2022-06-13 20:08:05 +0100

    dynamic featured product

--- components/partials/home/ProductCollection.vue
+++ components/partials/home/ProductCollection.vue
@@ -2,19 +2,29 @@
   <section class="grey-section pt-2 pb-8">
     <div class="container pb-4">
 
-      <ProductCollectionCart category-search="fashion"/>
+      <ProductCollectionCart v-for="category in categories" :key="category.slug" :category-search="category.slug"/>
 
-      <ProductCollectionCart category-search="informatique"/>
     </div>
   </section>
 </template>
 
 <script>
 import ProductCollectionCart from "~/components/product/ProductCollectionCart";
+import axios from "axios";
 
 export default {
   components: {
     ProductCollectionCart
   },
+  data() {
+    return {
+      categories: Array,
+    };
+  },
+  async fetch() {
+    const result = await axios.get(`http://localhost:3000/config/featured`);
+    this.categories = result.data.results.featured;
+    console.log(this.categories);
+  }
 };
 </script>
